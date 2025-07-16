f9e75042a669f19027bc49f10af238c1f8aecbe4
Hama <mohamedbenjedida18@gmail.com>
2025-07-16 15:07:23 +0100

    adding stop record

--- bigbluebutton-html5/imports/ui/components/recording/container.tsx
+++ bigbluebutton-html5/imports/ui/components/recording/container.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useMutation, useReactiveVar } from '@apollo/client';
 import RecordingComponent from './component';
 import { SET_RECORDING_STATUS } from './mutations';
@@ -22,9 +22,12 @@ const RecordingContainer: React.FC<RecordingContainerProps> = (props) => {
   } = props;
   const [setRecordingStatus] = useMutation(SET_RECORDING_STATUS);
   const connected = useReactiveVar(ConnectionStatus.getConnectedStatusVar());
+  const autoStopTimerRef = useRef<any>(null);
+
   const {
     data: recordingData,
   } = useDeduplicatedSubscription<GetRecordingResponse>(GET_MEETING_RECORDING_DATA);
+
   const {
     data: recordingPoliciesData,
   } = useDeduplicatedSubscription<GetRecordingPoliciesResponse>(GET_MEETING_RECORDING_POLICIES);
@@ -32,6 +35,40 @@ const RecordingContainer: React.FC<RecordingContainerProps> = (props) => {
   const recording = recordingData?.meeting_recording[0]?.isRecording ?? false;
   const time = recordingData?.meeting_recording[0]?.previousRecordedTimeInSeconds ?? 0;
   const allowStartStopRecording = recordingPoliciesData?.meeting_recordingPolicies[0]?.allowStartStopRecording ?? false;
+
+  // Auto-stop recording after 1 minute
+  useEffect(() => {
+    if (recording) {
+      // Clear any existing timer
+      if (autoStopTimerRef.current) {
+        clearTimeout(autoStopTimerRef.current);
+      }
+      
+      // Set timer to stop recording after 1 minute (60000 ms)
+      autoStopTimerRef.current = setTimeout(() => {
+        setRecordingStatus({
+          variables: {
+            recording: false,
+          },
+        });
+        setIsOpen(false);
+      }, 60000);
+    } else {
+      // Clear timer if recording is stopped manually
+      if (autoStopTimerRef.current) {
+        clearTimeout(autoStopTimerRef.current);
+        autoStopTimerRef.current = null;
+      }
+    }
+
+    // Cleanup function
+    return () => {
+      if (autoStopTimerRef.current) {
+        clearTimeout(autoStopTimerRef.current);
+        autoStopTimerRef.current = null;
+      }
+    };
+  }, [recording, setRecordingStatus, setIsOpen]);
 
   const toggleRecording = () => {
     setRecordingStatus({
