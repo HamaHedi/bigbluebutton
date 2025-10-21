643a5baf7a419d8f3fd4661831db43752769abf6
braiekhazem <braiekhazem@takiacademyteam.com>
2025-10-21 15:07:01 +0100

    fix emoji rain

--- bigbluebutton-html5/imports/ui/components/emoji-rain/component.jsx
+++ bigbluebutton-html5/imports/ui/components/emoji-rain/component.jsx
@@ -2,14 +2,19 @@ import React, { useRef, useState, useEffect } from 'react';
 import { getSettingsSingletonInstance } from '/imports/ui/services/settings';
 import Service from './service';
 import logger from '/imports/startup/client/logger';
+import useCurrentUser from '../../core/hooks/useCurrentUser';
 
 const EmojiRain = ({ reactions }) => {
+  const { data: currentUserData } = useCurrentUser((user) => ({
+    isModerator: user.isModerator,
+  }));
+  
   const Settings = getSettingsSingletonInstance();
   const containerRef = useRef(null);
   const [isAnimating, setIsAnimating] = useState(false);
   const EMOJI_SIZE = window.meetingClientSettings.public.app.emojiRain.emojiSize;
   const NUMBER_OF_EMOJIS = window.meetingClientSettings.public.app.emojiRain.numberOfEmojis;
-  const EMOJI_RAIN_ENABLED = window.meetingClientSettings.public.app.emojiRain.enabled || true;
+  const EMOJI_RAIN_ENABLED = currentUserData?.isModerator ? true : window.meetingClientSettings.public.app.emojiRain.enabled;
 
   const { animations } = Settings.application;
 
